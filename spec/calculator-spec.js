2398bfb762a8d1a8007e60a9c14cc4f49af12703
Yulia Shidlovskaya <sjullieb@gmail.com>
2019-03-23 16:12:59 -0700

    refactor methods and tests to calculate age on different planets

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -11,10 +11,16 @@ describe('multiplyAndRound', function(){
 describe('Calculator', function(){
   let birthday;
   let calc;
+  let gender;
+  let lifestyle;
+  let health;
 
   beforeEach(function(){
     birthday = new Date(2000, 8, 29);
     calc = new Calculator(birthday);
+    gender = 'male';
+    lifestyle = 5;
+    health = 5;
   });
 
   it('tests whether constructor saves the correct birthday', function(){
@@ -30,23 +36,33 @@ describe('Calculator', function(){
     expect(calc.calculateAge()).toEqual(18);
   });
 
-  it('tests whether Mercury age is calculated correctly', function(){
+  it('tests whether age on the planet is calculated correctly', function(){
+    let planet = 'Mercury';
     let mercuryAge = multiplyAndRound(calc.calculateAge(), .24);
-    expect(calc.calculateMercuryAge()).toEqual(mercuryAge);
+    expect(calc.calculateAgeOnPlanet(planet)).toEqual(mercuryAge);
   });
 
-  it('tests whether Venus age is calculated correctly', function(){
-    let venusAge = multiplyAndRound(calc.calculateAge(), .62);
-    expect(calc.calculateVenusAge()).toEqual(venusAge);
-  });
+  // it('tests whether Mercury age is calculated correctly', function(){
+  //   let mercuryAge = multiplyAndRound(calc.calculateAge(), .24);
+  //   expect(calc.calculateMercuryAge()).toEqual(mercuryAge);
+  // });
+  //
+  // it('tests whether Venus age is calculated correctly', function(){
+  //   let venusAge = multiplyAndRound(calc.calculateAge(), .62);
+  //   expect(calc.calculateVenusAge()).toEqual(venusAge);
+  // });
+  //
+  // it('tests whether Mars age is calculated correctly', function(){
+  //   let marsAge = multiplyAndRound(calc.calculateAge(), 1.88);
+  //   expect(calc.calculateMarsAge()).toEqual(marsAge);
+  // });
+  //
+  // it('tests whether Jupiter age is calculated correctly', function(){
+  //   let jupiterAge = multiplyAndRound(calc.calculateAge(), 11.86);
+  //   expect(calc.calculateJupiterAge()).toEqual(jupiterAge);
+  // });
 
-  it('tests whether Mars age is calculated correctly', function(){
-    let marsAge = multiplyAndRound(calc.calculateAge(), 1.88);
-    expect(calc.calculateMarsAge()).toEqual(marsAge);
-  });
-
-  it('tests whether Jupiter age is calculated correctly', function(){
-    let jupiterAge = multiplyAndRound(calc.calculateAge(), 11.86);
-    expect(calc.calculateJupiterAge()).toEqual(jupiterAge);
+  it('tests whether life expectancy calculates correctly', function(){
+    calculateLifeExpectancy(gender, health, lifestyle);
   });
 });
--- src/calculator.js
+++ src/calculator.js
@@ -1,3 +1,10 @@
+const planetConvertion = {
+  'Mercury' : .24,
+  'Venus' : .62,
+  'Mars' : 1.88,
+  'Jupiter': 11.86
+};
+
 export function multiplyAndRound (factor1, factor2){
   return Math.floor(factor1 * factor2).toFixed(0);
 }
@@ -18,19 +25,27 @@ export class Calculator{
     return age;
   }
 
-  calculateMercuryAge(){ // A Mercury year is .24 Earth year
-    return multiplyAndRound(this.calculateAge(this.birthday), 0.24);//Math.floor(this.calculateAge(this.birthday) * 0.24).toFixed(0);
+  calculateAgeOnPlanet(planet){
+    return multiplyAndRound(this.calculateAge(this.birthday), planetConvertion[planet]);
   }
 
-  calculateVenusAge(){ // A Venus year is .62 Earth year
-    return multiplyAndRound(this.calculateAge(this.birthday), 0.62);
-  }
+  // calculateMercuryAge(){ // A Mercury year is .24 Earth year
+  //   return multiplyAndRound(this.calculateAge(this.birthday), 0.24);//Math.floor(this.calculateAge(this.birthday) * 0.24).toFixed(0);
+  // }
+  //
+  // calculateVenusAge(){ // A Venus year is .62 Earth year
+  //   return multiplyAndRound(this.calculateAge(this.birthday), 0.62);
+  // }
+  //
+  // calculateMarsAge(){ // A Mars year is 1.88 Earth years
+  //   return multiplyAndRound(this.calculateAge(this.birthday), 1.88);
+  // }
+  //
+  // calculateJupiterAge(){ // A Jupiter year is 11.86 Earth years.
+  //   return multiplyAndRound(this.calculateAge(this.birthday), 11.86);
+  // }
 
-  calculateMarsAge(){ // A Mars year is 1.88 Earth years
-    return multiplyAndRound(this.calculateAge(this.birthday), 1.88);
-  }
-
-  calculateJupiterAge(){ // A Jupiter year is 11.86 Earth years.
-    return multiplyAndRound(this.calculateAge(this.birthday), 11.86);
+  calculateLifeExpectancy(gender, health, lifestyle){
+    return 90;
   }
 }
