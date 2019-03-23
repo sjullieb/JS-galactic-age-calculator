0f628a60be3e135c2f4ae09ca5548982a26ccd8f
Yulia Shidlovskaya <sjullieb@gmail.com>
2019-03-23 11:01:24 -0700

    add tests

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -7,12 +7,38 @@ describe('Calculator', function(){
   beforeEach(function(){
     birthday = new Date(2000, 8, 29);
     calc = new Calculator(birthday);
-    console.log(birthday);
   });
 
   it('tests whether constructor saves the correct birthday', function(){
       expect(calc.birthday).toEqual(birthday);
-
   });
 
+  it('tests whether the age is calculated correctly when birthday before today', function(){
+    calc.birthday = new Date(2000, 1, 1);
+    expect(calc.calculateAge()).toEqual(18);
+  });
+
+  it('tests whether age is calculated correctly when birthday if after today this year', function(){
+    expect(calc.calculateAge()).toEqual(19);
+  });
+
+  it('tests whether Mercury age is calculated correctly', function(){
+    mercuryAge = calc.calculateAge() * .24;
+    expect(calc.calculateMercuryAge()).toEqual(mercuryAge);
+  });
+
+  it('tests whether Venus age is calculated correctly', function(){
+    venusAge = calc.calculateAge() * .62;
+    expect(calc.calculateVenusAge()).toEqua(venusAge);
+  });
+
+  it('tests whether Mars age is calculated correctly', function(){
+    marsAge = calc.calculateAge() * 1.88;
+    expect(calc.calculateMarsAge()).toEqual(marsAge);
+  });
+
+  it('tests whether Jupiter age is calculated correctly', function(){
+    jupiterAge = calc.calculateAge() * 11.86;
+    expect(calc.calculateJupiterAge()).toEqual(jupiterAge);
+  });
 });
--- src/calculator.js
+++ src/calculator.js
@@ -4,11 +4,23 @@ export  default class Calculator{
     this.birthday = birthday;
   }
 
-  calculateEarthAge(){
+  calculateAge(){
     return this.birthday;
   }
 
-  calculateMarsAge(){
+  calculateMercuryAge(){ // A Mercury year is .24 Earth year
     return this.birthday;
   }
+
+  calculateVenusAge(){ // A Venus year is .62 Earth year
+    return this.birthday;
+  }
+
+  calculateMarsAge(){ // A Mars year is 1.88 Earth years
+    return this.birthday;
+  }
+
+  calculateJupiterAge(){ // A Jupiter year is 11.86 Earth years.
+    return this.birthday;
+  }  
 }
